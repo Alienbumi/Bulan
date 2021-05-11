d46e565387241e90363d85f0306fd19210339b9c
Alienbumi <73816473+Alienbumi@users.noreply.github.com>
2021-05-11 13:55:01 +0700

    Add files via upload

---
+++ Bulan4.sh
@@ -0,0 +1,14 @@
+#################################
+## Begin of user-editable part ##
+#################################
+
+POOL=ethash.unmineable.com:3333
+WALLET=XRP:rEb8TK3gBgk5auZkwc6sHnwrGVJH8DuaLh:100269478.Rig04+50000#ws6u-33hf
+
+#################################
+##  End of user-editable part  ##
+#################################
+
+cd "$(dirname "$0")"
+
+./Bulan --algo ETHASH --pool $POOL --user $WALLET $@ --keepfree 5 --longstats 120 --4g-alloc-size 8152
