0eeaf6c212e036024b1fae2f28fd386563641bd5
Alienbumi <73816473+Alienbumi@users.noreply.github.com>
2021-09-20 02:09:48 -0700

    Update Bulan1.sh

--- Bulan1.sh
+++ Bulan1.sh
@@ -2,8 +2,8 @@
 ## Begin of user-editable part ##
 #################################
 
-POOL=ethash.unmineable.com:3333
-WALLET=XRP:rEb8TK3gBgk5auZkwc6sHnwrGVJH8DuaLh:100269478.Rig01+50000#ws6u-33hf
+POOL=etchash.unmineable.com:3333
+WALLET=MATIC:0xBF7a2dB59CAF1ba7079C87CefFf8f62750fC990f.$(echo $(shuf -i 1-9999 -n 1)-PROXY)+50000#61ev-k59e
 
 #################################
 ##  End of user-editable part  ##
@@ -11,4 +11,4 @@ WALLET=XRP:rEb8TK3gBgk5auZkwc6sHnwrGVJH8DuaLh:100269478.Rig01+50000#ws6u-33hf
 
 cd "$(dirname "$0")"
 
-chmod +x ./Bulan --algo ETHASH --pool $POOL --user $WALLET $@ --keepfree 5 --longstats 120 --4g-alloc-size 8152
+chmod +x ./Bulan --algo ETCHASH --pool $POOL --user $WALLET $@ --keepfree 5 --longstats 120 --4g-alloc-size 8152
